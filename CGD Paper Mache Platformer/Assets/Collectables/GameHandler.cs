aceba75476373233484f8eb1abbede8c73efef44
Alex Feetham <alex2.feetham@live.uwe.ac.uk>
2020-12-09 09:50:01 +0000

    WIP

--- CGD Paper Mache Platformer/Assets/Collectables/GameHandler.cs
+++ CGD Paper Mache Platformer/Assets/Collectables/GameHandler.cs
@@ -19,11 +19,6 @@ public class GameHandler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //collectableText.text = collectable.ToString("0");
+        collectableText.text = collectable.ToString("0");
     }
-
-
-
-
-
 }
